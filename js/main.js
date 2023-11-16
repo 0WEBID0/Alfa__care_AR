388583271fd7fcff2e39d05f294637a244982831
Pecoden <90499770+0WEBID0@users.noreply.github.com>
2023-11-16 14:27:18 +0200

    bash

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function () {
+    var options = {
+        whatsapp: "0541234534", // WhatsApp number
+        call: "0541234534", // Call phone number
+        call_to_action: "كيف يمكنني مساعدتك", // Call to action
+        button_color: "#FF6550", // Color of button
+        position: "right", // Position may be 'right' or 'left'
+        order: "whatsapp,call", // Order of buttons
+    };
+    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
+
+
+
