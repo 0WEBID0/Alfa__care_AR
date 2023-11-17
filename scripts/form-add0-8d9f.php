66c628c87b72d42f4bb76634391e0ee2cf84ee9c
Pecoden <90499770+0WEBID0@users.noreply.github.com>
2023-11-17 10:23:05 +0200

    range

--- scripts/form-add0-8d9f.php
+++ scripts/form-add0-8d9f.php
@@ -8,34 +8,34 @@ $form = array(
     'success_redirect' => '',
     'sendIpAddress' => true,
     'email' => array(
-    'from' => 'email@gmail.com',
-    'to' => 'soholalfa@gmail.com',
-    'toCopy' => 'soholalfa@gmail.com',
-    'toHiddenCopy' => 'soholalfa@gmail.com'
+        'from' => 'email@gmail.com',
+        'to' => 'soholalfa@gmail.com',
+        'toCopy' => 'soholalfa@gmail.com',
+        'toHiddenCopy' => 'soholalfa@gmail.com'
     ),
     'fields' => array(
-    'name' => array(
-    'order' => 1,
-    'type' => 'string',
-    'label' => 'الاسم',
-    'required' => true,
-    'errors' => array(
-    'required' => 'Field \'الاسم\' is required.'
+        'name' => array(
+            'order' => 1,
+            'type' => 'string',
+            'label' => 'الاسم',
+            'required' => true,
+            'errors' => array(
+                'required' => 'Field \'الاسم\' is required.'
+            )
+        ),
+        'email' => array(
+            'order' => 2,
+            'type' => 'email',
+            'label' => 'Email',
+            'required' => true,
+            'errors' => array(
+                'required' => 'Field \'Email\' is required.'
+            )
+        ),
     )
-    ),
-    'email' => array(
-    'order' => 2,
-    'type' => 'email',
-    'label' => 'Email',
-    'required' => true,
-    'errors' => array(
-    'required' => 'Field \'Email\' is required.'
-    )
-    ),
-    )
-    );
+);
 
-    $processor = new FormProcessor('');
-    $processor->process($form);
+$processor = new FormProcessor('');
+$processor->process($form);
 
-    ?>
+?>
